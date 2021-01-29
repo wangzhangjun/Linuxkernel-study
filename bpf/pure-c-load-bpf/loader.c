de25619ed0fcd4026c19806ac4334977205f25d4
zhjwang <695331215@qq.com>
2021-01-29 14:49:22 +0800

    bpf lookup element in userspace

--- bpf/pure-c-load-bpf/loader.c
+++ bpf/pure-c-load-bpf/loader.c
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
     return -1;
   }
 
+  //插入元素--用户态
   int key,value,result;
   key = 1, value = 1234;
   result = bpf_map_update_elem(map_data[0].fd, &key, &value, BPF_ANY);//用户空间始终使用文件描述符来访问Map,放在全局的map_data数组中
@@ -19,6 +20,17 @@ int main(int argc, char **argv)
   }
   else{
     printf("userspace failed to update element:%d (%s)\n", result, strerror(errno));
+  }
+
+  //查找元素--用户态
+  int key1 = 1;
+  int value1;
+  result = bpf_map_lookup_elem(map_data[0].fd, &key1,&value1); //查找元素
+  if (result == 0){
+    printf("userspace lookup element success result:%d\n", value1);
+  }
+  else{
+    printf("userspace lookup element failed (%s)\n",strerror(errno));
   }
 
   read_trace_pipe();

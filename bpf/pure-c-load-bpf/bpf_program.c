31498f48d3282d3887d6e001497ef2c377954add
zhjwang <695331215@qq.com>
2021-01-29 14:44:35 +0800

    bpf lookup element in userspace

--- bpf/pure-c-load-bpf/bpf_program.c
+++ bpf/pure-c-load-bpf/bpf_program.c
@@ -15,15 +15,27 @@ int bpf_prog(void *ctx) {
   char msg[] = "Hello, BPF World!";
   bpf_trace_printk(msg, sizeof(msg));
   
+  //插入元素
   int key, value, result;
   key = 1, value = 1234;
   result = bpf_map_update_elem(&my_map, &key, &value, BPF_ANY); //BPF_ANY元素存在，内核更新元素，不存在，则在映射中创建该元素，同理可以尝试BPF_NOEXIST和BPF_EXIST
   if(result == 0) {
-    printt("update element success result:%d\n", result);
+    printt("kernel update element success result:%d\n", result);
   }else {
-    printt("failed to update element:%d\n", result);
+    printt("kernel update element failed:%d\n", result);
   }
-  
+
+  //查找元素
+  int key1 = 1;
+  int *value1;
+  value1 = bpf_map_lookup_elem(&my_map, &key1);  //查找元素
+  if (value1){
+    printt("kernel lookup element success result:%d\n", *value1);
+  }
+  else{
+    printt("kernel lookup element failed\n");
+  }
+
   return 0;
 }
 

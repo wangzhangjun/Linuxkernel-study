3947c0be0140f8af0788cbcae0b1c4b712280812
zhjwang <695331215@qq.com>
2021-01-29 15:23:02 +0800

    遍历map

--- bpf/pure-c-load-bpf/loader.c
+++ bpf/pure-c-load-bpf/loader.c
@@ -43,6 +43,37 @@ int main(int argc, char **argv)
     printf("userspace delete element failed (%s)\n", strerror(errno));
   }
 
+  //遍历元素
+  int new_key,new_value, it;
+  for(it=1; it < 6; it++) {
+    new_key = it;
+    new_value = 1234 + it;
+    bpf_map_update_elem(map_data[0].fd, &new_key, &new_value, BPF_ANY);//先添加几个
+  }
+  int next_key, lookup_key;
+  lookup_key = -1;
+  //每循环一个，会把下一个的key设置到next_key中。这里从-1开始就可以遍历所有的元素，如果是从中间的某一个，就是从中间的那一个到最后。
+  while (bpf_map_get_next_key(map_data[0].fd, &lookup_key, &next_key) == 0) {
+    printf("The next key in the map is: %d\n", next_key);
+    //如果正在遍历的时候，内核突然删除了一个，是什么效果,注意bpf_map_get_next_key将会重新开始查找
+    /*
+      效果如下：
+The next key in the map is: 1
+The next key in the map is: 4
+The next key in the map is: 5
+The next key in the map is: 2
+delteing 2
+The next key in the map is: 1
+The next key in the map is: 4
+The next key in the map is: 5
+The next key in the map is: 3
+    */
+    if(next_key == 2) {
+      printf("delteing 2\n");
+      bpf_map_delete_elem(map_data[0].fd, &next_key);
+    }
+    lookup_key = next_key;
+  }
   read_trace_pipe();
 
   return 0;
